064ce396e877ac5413b2f572cc08a94408fb83b2
coronado03 <sebascoronado03@hotmail.com>
2022-03-21 11:40:09 -0600

    Added validation for Contact form

--- components/contact.js
+++ components/contact.js
@@ -1,6 +1,19 @@
-import { Container, Button, Row, Form } from "react-bootstrap";
+import { Container, Button, Row, Form, Feedback } from "react-bootstrap";
+import { useState } from "react";
 
 export default function Contact() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <>
       <Container fluid id="contact" className="mt-3 bg-dark">
@@ -12,23 +25,35 @@ export default function Contact() {
             Always active for different types of jobs, I'm just one click away.
           </p>
           <Row></Row>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control type="textarea" placeholder="Name" />
+          <Form
+            noValidate
+            validated={validated}
+            method="post"
+            onSubmit={handleSubmit}
+          >
+            <Form.Group className="mb-3" controlId="validationCustom01">
+              <Form.Control required type="textarea" placeholder="Name" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control required type="email" placeholder="Email" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
               <Form.Control as="textarea" rows={3} placeholder="Message" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             </Form.Group>
+            <Button
+              type="submit"
+              className="rounded-pill"
+              variant="outline-light"
+            >
+              Send Message
+            </Button>
           </Form>
-          <Button className="rounded-pill" variant="outline-light">
-            Send Message
-          </Button>
         </Container>
       </Container>
     </>
